f8b1db1fd06064d826b58b680d3334c337868dc8
Leo <leooproencaaa@gmail.com>
2025-07-16 14:56:31 -0300

    start Carousel

--- src/app/_components/services.tsx
+++ src/app/_components/services.tsx
@@ -1,6 +1,17 @@
 export function Services() {
     return (
-        <section>
+        <section className="bg-white py-16 ">
+            <div className="container mx-auto px-4">
+             
+              
+                <h2 className="text-4xl font-bold mb-12">Serviços</h2>
+
+                <div className="relative">
+                    
+                </div>
+              
+
+            </div>
             
         </section>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { About } from "./_components/about";
 import { Hero } from "./_components/hero";
+import { Services } from "./_components/services";
 
 export default function Page() {
   return (
     <main>
       <Hero/>
       <About/>
+      <Services/>
     </main>
   )
 }
